657f8c59e3b7217a48f84de61928ff0786454dd0
Thulasidharan V S <76160415+thuluxx@users.noreply.github.com>
2026-08-03 20:04:07 +0530

    Time: 7 ms (78.00%) | Memory: 12.4 MB (18.05%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,13 +4,6 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        # a = 100
-        # b= a[0]
-        # print (b)
-        # if x[::-1] == x:
-        #     return true
-        # else:
-        #     return false
         str1 = str(x)
         if str1[::-1] == str1:
             return True
